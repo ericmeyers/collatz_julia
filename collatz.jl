8ed45227f6ba4fbd669b9a58393ab5fbd61ecaad
Eric Meyers <etm@ericmeyers.com>
2024-11-04 17:05:50 -0500

    do the first 5 million

--- collatz.jl
+++ collatz.jl
@@ -1,24 +1,33 @@
 #using Plots
 using GLMakie
-using IterTools
-using Base.Iterators
+#using IterTools
+#using Base.Iterators
+#using PlotlyJS
 
 
-
-
-function collatz(n :: Int64)
- if n % 2 == 0
-    div(n,2)
+function collatz_length_brian(n :: Int64)
+ if n==1
+     1
+ elseif n % 2 == 0
+    1 + collatz_length_brian(div(n,2))
  else
-    3 * n + 1
+    1 + collatz_length_brian(3*n + 1)
  end
 end
 
-
-function collatz_length(n :: Int64)
-    ans = collect(Int64,Iterators.takewhile(>(1),iterated(collatz, n)));
-    length(ans) + 1
-end
+#function collatz(n :: Int64)
+# if n % 2 == 0
+#    div(n,2)
+# else
+#    3 * n + 1
+# end
+#end
+#
+#
+#function collatz_length(n :: Int64)
+#    ans = collect(Int64,Iterators.takewhile(>(1),iterated(collatz, n)));
+#    length(ans) + 1
+#end
 
 
 
@@ -26,11 +35,7 @@ end
 #@time ans = [collect(Int64,Iterators.takewhile(>(1),it));1];
 #@show ans
 
-xs = range(start = 1, stop = 10000, step = 1)
-ys = map(collatz_length, xs)
-
-
-#GLMakie.closeall()
-
-f,ax = scatter(xs,ys)
-display(f)
+xs = range(start = 1, stop = 5000000, step = 1)
+ys = map(collatz_length_brian, xs)
+GLMakie.closeall()
+p=scatter(xs, ys)
